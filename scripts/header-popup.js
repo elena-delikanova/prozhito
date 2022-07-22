f8cc649671b1f7ae081a8142ffa1be6fd265b41b
OksanaSag <sagitovaoksana31@gmail.com>
2022-07-22 15:28:54 +0700

    added popup

---
+++ scripts/header-popup.js
@@ -0,0 +1,19 @@
+const buttonAdd = document.querySelector('.header__menu-button');
+const buttonClose = document.querySelector('.header__menu-button_close');
+const popups = document.querySelector('.header__popup')
+
+
+
+function openPopup(popup) {
+  popup.classList.add('header__popup_opened');
+}
+function closePopup(popup) {
+  popup.classList.remove('header__popup_opened');
+}
+
+buttonAdd.addEventListener('click', evt => {
+  openPopup(popups);
+});
+buttonClose.addEventListener('click', evt => {
+  closePopup(popups);
+});
