77aa5c1fbd14d3db6c332aad53422ad7becf1b3d
OksanaSag <sagitovaoksana31@gmail.com>
2022-07-24 11:41:34 +0700

    Squashed commit of the following:
    commit b3125c83b90d2abe1689a2de1e21d1e7337308ff
    Author: OksanaSag <91419571+OksanaSag@users.noreply.github.com>
    Date:   Fri Jul 22 19:27:02 2022 +0700
    
        Feat/lock (#32)
    
        * Desctop i adaptacia
    
        * pozicia popup
    
        * podkluchil js
    
        * margin, top - ispravleni
    
        Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@yandex.ru>
        Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>
        Co-authored-by: Александр Соколов <105075627+aleksandr-04-10@users.noreply.github.com>
    
    commit 48702d1ccf05452a4509ab245c15e27309b32706
    Author: Александр Соколов <105075627+aleksandr-04-10@users.noreply.github.com>
    Date:   Thu Jul 21 11:37:33 2022 +0300
    
        Feat/editor (#30)
    
        * editor pod desctop
    
        * adaptiroval pod planshet i mobil
    
        * knopki, otstupu i validator
    
        * текст и отступ
    
        Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>
    
    commit 71bb71a7c63bcc6a0e1ea6b1df59a6d4da75f8e0
    Author: Александр Соколов <105075627+aleksandr-04-10@users.noreply.github.com>
    Date:   Thu Jul 21 11:22:30 2022 +0300
    
        Revert "Feat/editor" (#29)
    
        * Feat/editor (#25)
    
        * [feat] основная структура (#2)
    
        * [feat] основная структура
    
        * [fix] color
    
        * [fix] упразднение папки src
    
        Co-authored-by: Elena Delikanova <elena_delikanova@yahoo.com>
    
        * [feat/video] Верстка блока видео (#17)
    
        * верстка видео
    
        * Убрала дефолтный margin figure
    
        * fix: убрано расстояние после видео, исправлен размер видео
    
        * fix: закомментировать путь к видео
    
        Co-authored-by: Catherine <cath.osipova@gmail.com>
    
        * Верстка секции с переходом по страницам (#12)
    
        * [feat] секция перехода по страницам
    
        * [fix] подписи к картинкам-кнопка
    
        * editor pod desctop
    
        * adaptiroval pod planshet i mobil
    
        * knopki, otstupu i validator
    
        * текст и отступ
    
        Co-authored-by: elena-delikanova <elena_delikanova@yahoo.com>
        Co-authored-by: Catherine <kata.ossipova@gmail.com>
        Co-authored-by: Catherine <cath.osipova@gmail.com>
        Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>
    
        * Revert "Feat/editor (#25)"
    
        This reverts commit e5e7c3fb1abf56be868cbd148104ccdafa8e9ffc.
    
        Co-authored-by: elena-delikanova <elena_delikanova@yahoo.com>
        Co-authored-by: Catherine <kata.ossipova@gmail.com>
        Co-authored-by: Catherine <cath.osipova@gmail.com>
        Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 const buttonAdd = document.querySelector('.header__menu-button');
 const buttonClose = document.querySelector('.header__menu-button_close');
-const popups = document.querySelector('.header__popup')
-
-
+const popups = document.querySelector('.header__popup');
+const popupLock = document.querySelector('.lock__popup');
+const lockCloseButton = document.querySelector('.lock__popup-close');
 
 function openPopup(popup) {
   popup.classList.add('header__popup_opened');
 }
 function closePopup(popup) {
   popup.classList.remove('header__popup_opened');
+}
+function togglePopup(popup) {
+  popup.classList.toggle('lock__popup_closed');
 }
 
 buttonAdd.addEventListener('click', evt => {
@@ -17,3 +20,4 @@ buttonAdd.addEventListener('click', evt => {
 buttonClose.addEventListener('click', evt => {
   closePopup(popups);
 });
+lockCloseButton.addEventListener( "click", () => togglePopup(popupLock));
