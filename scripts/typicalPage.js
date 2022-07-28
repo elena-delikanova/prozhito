7d19ac654bd8f8aea8ec0ac82da8b0d6058c8d33
aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>
2022-07-28 23:41:56 +0300

    oglavlenie i zametki

--- scripts/typicalPage.js
+++ scripts/typicalPage.js
@@ -1,3 +1,20 @@
 const pageTitle = document.querySelector('.title__text');
 const textBreadCrumbs = document.querySelector('.bread-crumbs__title');
 textBreadCrumbs.textContent = pageTitle.textContent;
+const popupTable = document.querySelector('.table');
+const popupNote = document.querySelector('.note')
+const tableCloseButton = document.querySelector('.table__close');
+const tableOpenButton = document.querySelector('.table-button__text');
+const noteCloseButton = document.querySelector('.note__close');
+
+function togglePopup(popup) {
+  popup.classList.toggle('table_opened');
+}
+
+function closeNotePopup(popup) {
+  popup.classList.toggle('lock__popup_closed');
+}
+
+tableCloseButton.addEventListener( "click", () => togglePopup(popupTable));
+tableOpenButton.addEventListener( "click", () => togglePopup(popupTable));
+noteCloseButton.addEventListener( "click", () => closeNotePopup(popupNote));
