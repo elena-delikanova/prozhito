c23d2c4c1f8f99b17cc08335422e0b084abc8678
Александр Соколов <105075627+aleksandr-04-10@users.noreply.github.com>
2022-07-29 10:13:45 +0300

    oglavlenie i zametki (#42)
    Co-authored-by: aleksandr-04-10 <aleksandr-04-10sokolov@andex.ru>

--- scripts/typicalPage.js
+++ scripts/typicalPage.js
@@ -1,6 +1,25 @@
 const pageTitle = document.querySelector('.title__text');
 const textBreadCrumbs = document.querySelector('.bread-crumbs__title');
 textBreadCrumbs.textContent = pageTitle.textContent;
+
+const popupTable = document.querySelector('.table');
+const popupNote = document.querySelector('.note')
+const tableCloseButton = document.querySelector('.table__close');
+const tableOpenButton = document.querySelector('.table-button__text');
+const noteCloseButton = document.querySelector('.note__close');
+
+function togglePopup(popup) {
+  popup.classList.toggle('table_opened');
+}
+
+function closeNotePopup(popup) {
+  popup.classList.toggle('lock__popup_closed');
+}
+
+tableCloseButton.addEventListener( "click", () => togglePopup(popupTable));
+tableOpenButton.addEventListener( "click", () => togglePopup(popupTable));
+noteCloseButton.addEventListener( "click", () => closeNotePopup(popupNote));
+
 const buttonAdd = document.querySelector('.header__menu-button');
 const buttonClose = document.querySelector('.header__menu-button_close');
 const popups = document.querySelector('.header__popup');
@@ -69,3 +88,4 @@ photoGallaries.forEach((gallary) => {
     }
   });
 });
+
